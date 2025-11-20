1c13bcbbb794fd3eafa96f633e9e6b4fec5af419
Deuk Mose <dewlonsystems@gmai.com>
2025-11-20 10:38:52 +0300

    added the welcome

--- backend/business_portal/urls.py
+++ backend/business_portal/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def root_view(request):
+    return HttpResponse("Welcome to Business Portal API")
 
 urlpatterns = [
     path('', root_view),
