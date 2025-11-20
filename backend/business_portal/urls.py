15cb28c0231079d462ebe8cc0deda86e96242a0a
Deuk Mose <dewlonsystems@gmai.com>
2025-11-20 10:30:52 +0300

    Fix / issue

--- backend/business_portal/urls.py
+++ backend/business_portal/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/payments/', include('payments.urls')),
